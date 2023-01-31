0e4fa15acc40b4d850c766b7ff51260d93e31989
Fonfazy <102817206+Kiryl88@users.noreply.github.com>
2023-01-31 11:06:51 +0100

    Task Manager

---
+++ TaskManager/src/test/java/fonfazy/takmanager/TakManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fonfazy.takmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
