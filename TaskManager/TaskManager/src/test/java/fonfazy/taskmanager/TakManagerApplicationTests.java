133883dc872470f49816c5ae838da8b38b94b7fa
Fonfazy <102817206+Kiryl88@users.noreply.github.com>
2023-01-31 11:09:20 +0100

    Add files via upload

---
+++ TaskManager/TaskManager/src/test/java/fonfazy/taskmanager/TakManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fonfazy.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
